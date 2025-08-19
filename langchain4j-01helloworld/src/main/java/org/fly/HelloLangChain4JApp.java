a8c34ba69807d0355f390528245c81170a35df81
fly9593 <2069036796@qq.com>
2025-08-19 13:55:05 +0800

    feat(langchain4j-01helloworld): 添加通义灵码配置和测试接口
    - 新增 LLMConfig 类，配置 OpenAiChatModel 以使用阿里云的 qwen-plus 模型
    - 添加 HelloLangChain4JController 控制器，实现简单的聊天接口- 在 application.properties 中保留现有的配置项

--- langchain4j-01helloworld/src/main/java/org/fly/HelloLangChain4JApp.java
+++ langchain4j-01helloworld/src/main/java/org/fly/HelloLangChain4JApp.java
@@ -7,10 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class HelloLangChain4JApp
-{
-    public static void main(String[] args)
-    {
-        SpringApplication.run(HelloLangChain4JApp.class,args);
+public class HelloLangChain4JApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloLangChain4JApp.class, args);
     }
 }
---
+++ langchain4j-01helloworld/src/main/java/org/fly/controller/HelloLangChain4JController.java
@@ -0,0 +1,28 @@
+package org.fly.controller;
+
+import dev.langchain4j.model.chat.ChatModel;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloLangChain4JController {
+
+    @Resource
+    private ChatModel chatModel;
+
+    // http://localhost:9001/langchain4j/hello?question=给我讲一些有趣的事情
+
+
+    @GetMapping(value = "/langchain4j/hello")
+    public String hello(@RequestParam(value = "question", defaultValue = "你是谁") String question) {
+        String result = chatModel.chat(question);
+
+        System.out.println("调用大模型回复: " + result);
+
+        return result;
+    }
+}
