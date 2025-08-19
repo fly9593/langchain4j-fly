4b6ed5120d835cc43e3c3108a452b955534e5e1e
fly9593 <2069036796@qq.com>
2025-08-19 09:58:50 +0800

    init

---
+++ langchain4j-01helloworld/src/main/java/org/fly/HelloLangChain4JApp.java
@@ -0,0 +1,16 @@
+package org.fly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class HelloLangChain4JApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HelloLangChain4JApp.class,args);
+    }
+}
